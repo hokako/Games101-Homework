b2d8da12bc80975ccbbc56013399bfa4703f37d8
hokako <hinanawihoka@gmail.com>
2025-12-06 21:04:17 +0800

    2

---
+++ Assignment2/build/CMakeFiles/Rasterizer2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rasterizer2.dir/Triangle.cpp.o"
+  "CMakeFiles/Rasterizer2.dir/Triangle.cpp.o.d"
+  "CMakeFiles/Rasterizer2.dir/main.cpp.o"
+  "CMakeFiles/Rasterizer2.dir/main.cpp.o.d"
+  "CMakeFiles/Rasterizer2.dir/rasterizer.cpp.o"
+  "CMakeFiles/Rasterizer2.dir/rasterizer.cpp.o.d"
+  "Rasterizer2"
+  "Rasterizer2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rasterizer2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
